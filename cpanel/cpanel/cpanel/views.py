5d87800a7f0a24fbc37284ea466b0a7bf78efc64
fuhu <fuhu@umich.edu>
2018-10-13 06:08:10 -0400

    Now it looks like all works

--- cpanel/cpanel/cpanel/views.py
+++ cpanel/cpanel/cpanel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import pyrebase
+from django.contrib import auth
 
 config = {
 
@@ -16,10 +17,9 @@ firebase = pyrebase.initialize_app(config)
 authe = firebase.auth()
 database=firebase.database()
 def signIn(request):
-    
     return render(request, "signIn.html")
+
 def postsign(request):
-    
     email=request.POST.get('email')
     passw = request.POST.get("pass")
     try:
@@ -30,11 +30,7 @@ def postsign(request):
     print(user['idToken'])
     session_id=user['idToken']
     request.session['uid']=str(session_id)
-    if not 'uid' in request.session:
-        return render(request, 'signIn.html')
-    else:
-        return render(request, "welcome.html",{"e":email})
-
+    return render(request, "welcome.html",{"e":email})
 def logout(request):
     auth.logout(request)
     return render(request,'signIn.html')
@@ -43,8 +39,6 @@ def logout(request):
 def signUp(request):
     
     return render(request,"signup.html")
-
-
 def postsignup(request):
     
     name=request.POST.get('name')
@@ -55,7 +49,7 @@ def postsignup(request):
     except:
         message="Unable to create account try again"
         return render(request,"signup.html",{"messg":message})
-    uid = user['localId']
+        uid = user['localId']
     
     data={"name":name,"status":"1"}
 
@@ -63,78 +57,110 @@ def postsignup(request):
     return render(request,"signIn.html")
 
 def create(request):
-    if not 'uid' in request.session:
-        return render(request, 'signIn.html')
-    else:
-        
-        return render(request,'create.html')
+    
+    return render(request,'create.html')
+
 
 def post_create(request):
-    if not 'uid' in request.session:
-        return render(request, 'signIn.html')
-    else:
-        import time
-        from datetime import datetime, timezone
-        import pytz
-        
-        tz= pytz.timezone('Asia/Kolkata')
-        time_now= datetime.now(timezone.utc).astimezone(tz)
-        millis = int(time.mktime(time_now.timetuple()))
-        print("mili"+str(millis))
-        work = request.POST.get('work')
-        progress =request.POST.get('progress')
-        url = request.POST.get('url')
-        idtoken= request.session['uid']
-        a = authe.get_account_info(idtoken)
-        a = a['users']
-        a = a[0]
-        a = a['localId']
-        print("info"+str(a))
-        data = {
-            "work":work,
-            'progress':progress,
-            'url':url
-        }
-        database.child('users').child(a).child('reports').child(millis).set(data)
-        name = database.child('users').child(a).child('details').child('name').get().val()
-        push_notify(name,progress,work)
-        return render(request,'welcome.html', {'e':name})
-
-def check(request):
-    import datetime
-    idtoken = request.session['uid']
+    
+    import time
+    from datetime import datetime, timezone
+    import pytz
+    
+    tz= pytz.timezone('Asia/Kolkata')
+    time_now= datetime.now(timezone.utc).astimezone(tz)
+    millis = int(time.mktime(time_now.timetuple()))
+    print("mili"+str(millis))
+    work = request.POST.get('work')
+    progress =request.POST.get('progress')
+    url = request.POST.get('url')
+    idtoken= request.session['uid']
     a = authe.get_account_info(idtoken)
     a = a['users']
     a = a[0]
     a = a['localId']
+    print("info"+str(a))
+    data = {
+        "work":work,
+        'progress':progress,
+        'url':url
+    }
+    database.child('users').child(a).child('reports').child(millis).set(data)
+    name = database.child('users').child(a).child('details').child('name').get().val()
+    return render(request,'welcome.html', {'e':name})
+
+def check(request):
     
-    timestamps = database.child('users').child(a).child('reports').shallow().get().val()
-    lis_time=[]
-    for i in timestamps:
+    if request.method == 'GET' and 'csrfmiddlewaretoken' in request.GET:
+        search = request.GET.get('search')
+        search = search.lower()
+        uid = request.GET.get('uid')
+        print(search)
+        print(uid)
+        timestamps = database.child('users').child(uid).child('reports').shallow().get().val()
+        work_id=[]
+        for i in timestamps:
+            wor = database.child('users').child(uid).child('reports').child(i).child('work').get().val()
+            wor = str(wor)+"$"+str(i)
+            work_id.append(wor)
+        matching = [str(string) for string in work_id if search in string.lower()]
         
-        lis_time.append(i)
-        lis_time.sort(reverse=True)
+        s_work=[]
+        s_id=[]
+        
+        for i in matching:
+            work,ids=i.split('$')
+            s_work.append(work)
+            s_id.append(ids)
+        print(s_work)
+        print(s_id)
+        date = []
+        import datetime
+        for i in s_id:
+            i = float(i)
+            dat = datetime.datetime.fromtimestamp(i).strftime('%H:%M %d-%m-%Y')
+            date.append(dat)
+        comb_lis = zip(s_id, date, s_work)
+        name = database.child('users').child(uid).child('details').child('name').get().val()
 
-    print(lis_time)
-    work = []
+        return render(request, 'check.html', {'comb_lis': comb_lis, 'e': name, 'uid': uid})
 
-    for i in lis_time:
-        wor=database.child('users').child(a).child('reports').child(i).child('work').get().val()
-        work.append(wor)
-    print(work)
+    else:
+        import datetime
+        idtoken = request.session['uid']
+        a = authe.get_account_info(idtoken)
+        a = a['users']
+        a = a[0]
+        a = a['localId']
+        
+        timestamps = database.child('users').child(a).child('reports').shallow().get().val()
+        lis_time=[]
+        for i in timestamps or []:
+            
+            lis_time.append(i)
     
-    date=[]
-    for i in lis_time:
-        i = float(i)
-        dat = datetime.datetime.fromtimestamp(i).strftime('%H:%M %d-%m-%Y')
-        date.append(dat)
+        lis_time.sort(reverse=True)
+        
+        print(lis_time)
+        work = []
+        
+        for i in lis_time:
+            wor=database.child('users').child(a).child('reports').child(i).child('work').get().val()
+            work.append(wor)
+        print(work)
+        
+        date=[]
+        for i in lis_time:
+            i = float(i)
+            dat = datetime.datetime.fromtimestamp(i).strftime('%H:%M %d-%m-%Y')
+            date.append(dat)
 
     print(date)
 
     comb_lis = zip(lis_time,date,work)
     name = database.child('users').child(a).child('details').child('name').get().val()
 
-    return render(request,'check.html',{'comb_lis':comb_lis,'e':name})
+    return render(request,'check.html',{'comb_lis':comb_lis,'e':name,'uid':a})
 
 def post_check(request):
     
@@ -157,17 +183,3 @@ def post_check(request):
     name = database.child('users').child(a).child('details').child('name').get().val()
     
     return render(request,'post_check.html',{'w':work,'p':progress,'d':dat,'e':name,'i':img_url})
-
-def push_notify(name,progress,work):
-    from pusher_push_notifications import PushNotifications
-    
-    pn_client = PushNotifications(
-                                  instance_id='be2dce08-9b23-4a81-a72b-fab39797530c',
-                                  secret_key='0D2A5BABBF132520C4CF8FB9C165D12',
-                                  )
-    response = pn_client.publish(
-                                 interests=['hello'],
-                                 publish_body={'apns': {'aps': {'alert': 'Report Created'}}, 'fcm': {'notification': {'title': str(name), 'body': 'Progress: '+str(progress) +" work: " +str(work) }}}
-                                 )
-                                  
-    print(response['publishId'])
